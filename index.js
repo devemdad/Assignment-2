9d43b58a5e92a3734eb7438674bfdb0b007a09bd
MD EMDADUL HAQUE <156192762+devemdad@users.noreply.github.com>
2024-12-12 23:26:33 +0600

    Calulat isOdd

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function calculateDifference(p, q) {
     return p - q;
 }
 console.log(calculateDifference(10, 5));
+
+
+// 2 No Question
+function isOdd(x){
+    if(x % 2 !==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(isOdd(11));
